4b9dd016e984b87b67223da3db6a03ffead527d2
Liam Woodleigh-Hardinge <liam@liamwh.com>
2023-08-07 00:40:18 +0200

    remove yanky

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,7 +25,6 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.coding.copilot" },
         { import = "lazyvim.plugins.extras.util.project" },
         { import = "lazyvim.plugins.extras.dap.core" },
-        { import = "lazyvim.plugins.extras.coding.yanky" },
 
         -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
         -- import/override with your plugins
