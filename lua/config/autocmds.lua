281d18a7a55faffe8b844438c019424c158cbfe8
Liam Woodleigh <liam.woodleigh@gmail.com>
2023-08-16 21:12:44 +0200

    Proper fix for wsl clipboard

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,13 +3,19 @@
 -- Add any additional autocmds here
 vim.opt.clipboard = "unnamedplus"
 
+-- I installed win32yank using scoop, then symlinked it to /usr/local/bin
+-- sudo ln -s "/mnt/c/Users/YourUsername/scoop/shims/win32yank.exe" "/usr/local/bin/win32yank"
 if vim.fn.has("wsl") == 1 then
-    vim.api.nvim_create_autocmd("TextYankPost", {
-
-        group = vim.api.nvim_create_augroup("Yank", { clear = true }),
-
-        callback = function()
-            vim.fn.system("clip.exe", vim.fn.getreg('"'))
-        end,
-    })
+    vim.g.clipboard = {
+        name = "wslclipboard",
+        copy = {
+            ["+"] = "/usr/local/bin/win32yank -i --crlf",
+            ["*"] = "/usr/local/bin/win32yank -i --crlf",
+        },
+        paste = {
+            ["+"] = "/usr/local/bin/win32yank -o --lf",
+            ["*"] = "/usr/local/bin/win32yank -o --lf",
+        },
+        cache_enabled = 1,
+    }
 end
