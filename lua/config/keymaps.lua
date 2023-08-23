5387a04c25add71fa740a961e1df29e4e98969c4
Liam Woodleigh <liam.woodleigh@gmail.com>
2023-08-23 10:38:42 +0200

    Black hole register deleted things

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,5 @@
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("x", "<Up>", ":move '<-2<CR>gv-gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("x", "<Down>", ":move '>+1<CR>gv-gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "dd", '"_dd', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "di", '"_di', { noremap = true, silent = true })
