a0f637e6b4b1553aa2f82e1f21c5b738c5291db8
Liam Woodleigh-Hardinge <liam@liamwh.com>
2023-08-24 12:17:48 +0200

    Added d to go to the black hole register

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,6 @@
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("x", "<Up>", ":move '<-2<CR>gv-gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("x", "<Down>", ":move '>+1<CR>gv-gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "d", '"_d', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "dd", '"_dd', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "di", '"_di', { noremap = true, silent = true })
