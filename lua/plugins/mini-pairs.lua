4de7d8ce1172f2fe197af2ba6204e4562cea59ab
Liam Woodleigh <liam.woodleigh@gmail.com>
2023-10-01 18:06:24 +0200

    Custom mini.pairs config

---
+++ lua/plugins/mini-pairs.lua
@@ -0,0 +1,36 @@
+return {
+    {
+        "echasnovski/mini.pairs",
+        enabled = true,
+        opts = {
+            mappings = {
+                ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+                ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+                ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+
+                [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+                ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+                ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+
+                ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+                ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+                ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+            },
+        },
+        keys = {
+            {
+                "<leader>up",
+                function()
+                    local Util = require("lazy.core.util")
+                    vim.g.minipairs_disable = not vim.g.minipairs_disable
+                    if vim.g.minipairs_disable then
+                        Util.warn("Disabled auto pairs", { title = "Option" })
+                    else
+                        Util.info("Enabled auto pairs", { title = "Option" })
+                    end
+                end,
+                desc = "Toggle auto pairs",
+            },
+        },
+    },
+}
