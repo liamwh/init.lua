f4b73ecc8c9aeece0d4202de33fb4d594cc36e6d
Liam Woodleigh <liam.woodleigh@gmail.com>
2023-10-01 18:06:48 +0200

    Remove comment

--- lua/plugins/surreal.lua
+++ lua/plugins/surreal.lua
@@ -2,7 +2,6 @@ return {
     "dariuscorvus/tree-sitter-surrealdb.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
-        -- setup step
         require("tree-sitter-surrealdb").setup()
     end,
 }
