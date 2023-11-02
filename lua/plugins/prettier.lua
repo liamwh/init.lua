1b4a928f51232bb01501c1bd87dc98b1c65d1690
Liam Woodleigh <liam.woodleigh@gmail.com>
2023-11-02 15:58:04 +0100

    Ensure powershell editor services installed

--- lua/plugins/prettier.lua
+++ lua/plugins/prettier.lua
@@ -14,6 +14,7 @@ return {
             ensure_installed = {
                 "prettierd",
                 "codelldb",
+                "powershell-editor-services",
             },
         },
     },
