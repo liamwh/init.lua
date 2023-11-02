fda7250fd71ce0bbd8af4a1e71654128f419d4ea
Liam Woodleigh <liam.woodleigh@gmail.com>
2023-11-02 17:45:05 +0100

    Clarify for Rust docs

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -3,7 +3,7 @@ return {
     config = function()
         require("gen").model = "codellama"
         require("gen").prompts["Document_Code"] = {
-            prompt = "Add doc comments to the following code. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
+            prompt = "Add doc comments to the following code. In Rust files, they are denoted by `///`. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
             replace = true,
             extract = "```$filetype\n(.-)```",
         }
