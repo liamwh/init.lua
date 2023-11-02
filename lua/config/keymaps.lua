bf2a61f0e2e573095ba7df3b73e33b9c89886fab
Liam Woodleigh-Hardinge <liam@liamwh.com>
2023-11-02 16:23:29 +0100

    Add keymaps and document prompt to gen.llm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,6 @@ vim.api.nvim_set_keymap("n", "dd", '"_dd', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "di", '"_di', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>gb", ":Git blame<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>mp", ":MarkdownPreview<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<leader>l", ":Gen<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>l", ":Gen<CR>", { noremap = true, silent = true })
--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -1,3 +1,11 @@
 return {
     "https://github.com/David-Kunz/gen.nvim",
+    config = function()
+        require("gen").model = "codellama"
+        require("gen").prompts["Document_Code"] = {
+            prompt = "Add doc comments to the following code. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
+            replace = true,
+            extract = "```$filetype\n(.-)```",
+        }
+    end,
 }
