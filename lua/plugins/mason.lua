cea4f9cda98206140999e19c973b98464faa6faa
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-01-16 14:10:42 +0100

    fix: Ensure required components are installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,6 +6,8 @@ return {
             "codelldb",
             "powershell-editor-services",
             "docker-compose-language-service",
+            "svelte-language-server",
+            "helm-ls",
         },
     },
 }
