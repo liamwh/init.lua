bba37be6f1df93e802079bc5c44d50d056388898
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-02-21 10:35:49 +0100

    fix: Add localleader l for loading an agda file

--- lua/plugins/agda.lua
+++ lua/plugins/agda.lua
@@ -1,10 +1,19 @@
 return {
-    "https://github.com/ashinkarov/nvim-agda.git",
-    config = function()
-        local utf8 = require 'lua-utf8'
-        vim.g.nvim_agda_settings = {
-            agda = "/usr/bin/agda",
-            debug_p = true
-        }
-    end,
+    -- "https://github.com/ashinkarov/nvim-agda.git",
+    -- config = function()
+    --     local utf8 = require 'lua-utf8'
+    --     vim.g.nvim_agda_settings = {
+    --         agda = "/usr/bin/agda",
+    --         debug_p = true
+    --     }
+    -- end,
+    {
+        'isti115/agda.nvim',
+        dependencies = { "nvim-lua/plenary.nvim" },
+        vim.api.nvim_set_keymap("n", "<localleader>l", ":lua require('agda').load()<CR>",
+            { noremap = true, silent = true })
+    }
+    -- {
+    --     "https://github.com/isovector/cornelis.git"
+    -- }
 }
