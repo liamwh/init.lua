813ddc2fde0c2c7cf3ef3a2560bce1539bcc2c7a
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-02-21 09:22:40 +0100

    style: Formatting

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -1,16 +1,16 @@
 return {
-    {
-        "https://github.com/David-Kunz/gen.nvim",
-        config = function()
-            require("gen").model = "mistral"
-            require("gen").prompts["Document_Code"] = {
-                prompt =
-                "Add doc comments to the following code. In Rust files, they are denoted by `///`. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
-                replace = true,
-                extract = "```$filetype\n(.-)```",
-            }
-        end,
-    },
+    -- {
+    --     "https://github.com/David-Kunz/gen.nvim",
+    --     config = function()
+    --         require("gen").model = "mistral"
+    --         require("gen").prompts["Document_Code"] = {
+    --             prompt =
+    --             "Add doc comments to the following code. In Rust files, they are denoted by `///`. Only ouput the result in format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
+    --             replace = true,
+    --             extract = "```$filetype\n(.-)```",
+    --         }
+    --     end,
+    -- },
     {
         'stevearc/dressing.nvim',
         lazy = true,
