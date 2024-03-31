dffd53579727e7e09d402ae9fd05f103d25796da
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-03-31 11:46:59 +0200

    feat: Add key binding to open docs.rs for symbol under cursor

---
+++ lua/plugins/rust.lua
@@ -0,0 +1,8 @@
+return {
+    {
+        "mrcjkb/rustaceanvim",
+        keys = {
+            { "<leader>cd", "<cmd>RustLsp openDocs<CR>", desc = "Open Docs.rs for Symbol" },
+        },
+    },
+}
