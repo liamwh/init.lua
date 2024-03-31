b5b741430aa306ca14ee1968f0789a97103fb253
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-03-31 12:35:49 +0200

    feat: Improve the way ripgrep searches

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,45 @@
 return {
     "nvim-telescope/telescope.nvim",
-    opts = {
-        pickers = {
-            find_files = {
-                reuse_win = true,
+    opts = function(_, opts)
+        local telescopeConfig = require("telescope.config")
+
+        -- Clone the default Telescope configuration
+        local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+        -- I want to search in hidden/dot files.
+        table.insert(vimgrep_arguments, "--hidden")
+
+        -- I don't want to search in the `.git` directory.
+        table.insert(vimgrep_arguments, "--glob")
+        table.insert(vimgrep_arguments, "!**/.git/*")
+
+        -- I don't want to search in `.env` files.
+        table.insert(vimgrep_arguments, "--glob")
+        table.insert(vimgrep_arguments, "!**.env")
+
+        opts.pickers = opts.pickers or {}
+        opts.pickers.defaults = {
+            vimgrep_arguments = vimgrep_arguments,
+        }
+        opts.pickers.find_files = {
+            reuse_win = true,
+            find_command = {
+                "rg",
+                "--files",
+                "--hidden",
+                "--glob",
+                "!**/.git/*",
+                "--glob",
+                "!**.env"
             },
-            live_grep = {
-                reuse_win = true,
-            },
-            buffers = {
-                reuse_win = true,
-            },
-        },
-    },
+        }
+        opts.pickers.live_grep = {
+            reuse_win = true,
+        }
+        opts.pickers.buffers = {
+            reuse_win = true,
+        }
+
+        return opts
+    end,
 }
