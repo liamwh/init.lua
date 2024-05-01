3bcf59d104d2b336fd16420bcc1c2f1b1f75ae10
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-05-01 09:51:57 +0200

    fix(llm): Tweak Ollama prompt keybind

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -37,17 +37,16 @@ return {
         keys = {
             -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
             {
-                "<leader>oo",
+                "<leader>op",
                 ":<c-u>lua require('ollama').prompt()<cr>",
-                desc = "ollama prompt",
+                desc = "Ollama prompt",
                 mode = { "n", "v" },
             },
-
             -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
             {
                 "<leader>oG",
                 ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
-                desc = "ollama Generate Code",
+                desc = "Ollama Generate Code",
                 mode = { "n", "v" },
             },
         },
