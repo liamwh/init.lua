9bd3398d5aa8956f267b0710ee4358ae956cdd16
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-05-08 14:23:59 +0200

    fix: Comment out zen-mode

--- lua/plugins/twilight.lua
+++ lua/plugins/twilight.lua
@@ -3,17 +3,17 @@ return {
         "folke/twilight.nvim",
         enabled = false,
     },
-    {
-        "folke/zen-mode.nvim",
-        enabled = false,
-        cmd = "ZenMode",
-        opts = {
-            plugins = {
-                gitsigns = true,
-                tmux = true,
-                kitty = { enabled = false, font = "+2" },
-            },
-        },
-        keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-    },
+    -- {
+    --     "folke/zen-mode.nvim",
+    --     enabled = false,
+    --     cmd = "ZenMode",
+    --     opts = {
+    --         plugins = {
+    --             gitsigns = true,
+    --             tmux = true,
+    --             kitty = { enabled = false, font = "+2" },
+    --         },
+    --     },
+    --     keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+    -- },
 }
