ce628a7cef0124961024a945fe103178b5d7a918
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-05-28 09:32:44 +0200

    fix: Rustaceanvim conflicting with rust analyzer

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -5,4 +5,15 @@ return {
             { "<leader>cd", "<cmd>RustLsp openDocs<CR>", desc = "Open Docs.rs for Symbol" },
         },
     },
+    {
+        -- the below is recommended by rustaceanvim
+        "neovim/nvim-lspconfig",
+        opts = {
+            setup = {
+                rust_analyzer = function()
+                    return true
+                end,
+            },
+        },
+    },
 }
