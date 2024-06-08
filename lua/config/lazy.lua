2e46c9e0f42cbf3991377172e2be86f16e0bcc60
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-06-08 17:34:42 +0200

    feat: Add clojure support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -40,6 +40,8 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.tex" },
         { import = "lazyvim.plugins.extras.lang.helm" },
         { import = "lazyvim.plugins.extras.lang.haskell" },
+        { import = "lazyvim.plugins.extras.lang.clojure" },
+        -- { import = "lazyvim.plugins.extras.lang.php" },
         -- { import = "lazyvim.plugins.extras.lang.ruby" },
         -- linting/formatting
         { import = "lazyvim.plugins.extras.linting.eslint" },
