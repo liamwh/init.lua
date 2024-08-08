815a811e810e2f7198d4f65ed2a6fc5077548364
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-08-08 16:34:18 +0200

    feat(fzf): fix keybindings

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -4,8 +4,13 @@ return {
         local actions = require("fzf-lua.actions")
         opts.grep = vim.tbl_deep_extend("force", opts.grep, {
             actions = {
-                ["ctrl-g"] = false,
-                ["alt-g"] = actions.grep_lgrep,
+                -- ["ctrl-g"] = false,
+                -- ["alt-g"] = actions.grep_lgrep,
+                ["alt-g"] = false,
+                ["alt-i"] = false,
+                ["alt-h"] = false,
+                ["ctrl-i"] = actions.toggle_ignore,
+                ["alt-d"] = actions.toggle_hidden,
             },
         })
     end,
