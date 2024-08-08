7aaa38dd2daf7a2f44cca7c55bb181a346d4be18
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-08-08 16:35:17 +0200

    fix(haskell): Disable haskell for now

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -39,7 +39,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.yaml" },
         { import = "lazyvim.plugins.extras.lang.tex" },
         { import = "lazyvim.plugins.extras.lang.helm" },
-        { import = "lazyvim.plugins.extras.lang.haskell" },
+        -- { import = "lazyvim.plugins.extras.lang.haskell" },
         -- { import = "lazyvim.plugins.extras.lang.clojure" },
         -- { import = "lazyvim.plugins.extras.lang.php" },
         -- { import = "lazyvim.plugins.extras.lang.ruby" },
