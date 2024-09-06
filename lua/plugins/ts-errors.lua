68580b3ce690a34dc61db42eb7fa2c0d6dbfd8e4
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-09-06 17:44:40 +0200

    feat(nvim): Better ts plugin

---
+++ lua/plugins/ts-errors.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "OlegGulevskyy/better-ts-errors.nvim",
+        dependencies = { "MunifTanjim/nui.nvim" },
+        config = {
+            keymaps = {
+                toggle = "<leader>dd",
+            },
+        },
+    },
+    {
+        "MunifTanjim/nui.nvim",
+        lazy = true,
+    },
+}
