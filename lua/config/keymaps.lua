dc082bd03a4c1d4d3022242f11cb3f92bf562d06
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2025-09-28 00:43:25 +0200

    fix(Cursor): Fix inputting text in certain windows in cursor with changes to nvim keybindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -102,3 +102,68 @@ end
 -- Save with ⌘ + S
 vim.api.nvim_set_keymap('n', '<D-s>', ':w<CR>', { noremap = true, silent = true })
 
+----------------------------
+-- VS Code + Neovim: re-create VS Code "space ..." chords inside Neovim
+-- This prevents VS Code's chord engine from ever engaging in chat inputs.
+if not vim.g.vscode then return end
+
+vim.g.mapleader = " "
+local map = vim.keymap.set
+local function cmd(c) return function() vim.fn.VSCodeNotify(c) end end
+local opts = { noremap = true, silent = true }
+
+-- Files / switching
+map('n', '<leader>,',           cmd('workbench.action.showAllEditors'), opts)
+map('n', '<leader><leader>',    cmd('workbench.action.quickOpen'),      opts)
+map('n', '<leader>bb',          cmd('workbench.action.quickOpenPreviousRecentlyUsedEditorInGroup'), opts)
+
+-- Search / navigation
+map('n', '<leader>/',           cmd('workbench.action.findInFiles'),    opts)
+map('n', '<leader>ss',          cmd('workbench.action.showAllSymbols'), opts)
+
+-- Window management
+map('n', '<leader>bd',          cmd('workbench.action.closeActiveEditor'), opts)
+map('n', '<leader>v',           cmd('workbench.action.splitEditorRight'),  opts)
+map('n', '<leader>h',           cmd('workbench.action.splitEditorDown'),   opts)
+map('n', '<leader>-',           cmd('workbench.action.splitEditorDown'),   opts)
+map('n', '<leader>wd',          cmd('workbench.action.closeEditorsInGroup'), opts)
+
+-- Explorer / sidebars
+map('n', '<leader>e',           cmd('workbench.view.explorer'),         opts)
+map('n', '<leader>fm',          cmd('workbench.view.explorer'),         opts)
+map('n', '<leader>tt',          cmd('workbench.action.toggleSidebarVisibility'), opts)
+
+-- Code actions / refs / rename
+map('n', '<leader>ca',          cmd('editor.action.quickFix'),          opts)
+map('n', '<leader>cr',          cmd('editor.action.rename'),            opts)
+map('n', '<leader>gr',          cmd('editor.action.referenceSearch.trigger'), opts)
+
+-- Testing
+map('n', '<leader>ts',          cmd('workbench.view.testing.focus'),    opts)
+
+-- Markdown
+map('n', '<leader>mp',          cmd('markdown.showPreviewToSide'),      opts)
+
+-- Outline / timeline
+map('n', '<leader>so',          cmd('outline.focus'),                   opts)
+map('n', '<leader>u',           cmd('timeline.focus'),                  opts)
+map('n', '<leader>fh',          cmd('timeline.focus'),                  opts)
+
+-- Find/replace
+map('n', '<leader>sr',          cmd('editor.action.startFindReplaceAction'), opts)
+
+-- Git / GitLens / Lazygit wrapper
+map('n', '<leader>gg',          cmd('extension.multiCommand.closeSidebarAndOpenLazyGit'), opts)
+map('n', '<leader>gbl',         cmd('gitlens.toggleLineBlame'),         opts)
+map('n', '<leader>gbf',         cmd('gitlens.toggleFileBlame'),         opts)
+map('n', '<leader>gh',          cmd('workbench.scm.history.focus'),     opts)
+
+-- Problems / diagnostics navigation
+map('n', '<leader>pn',          cmd('editor.action.marker.next'),       opts)
+map('n', '<leader>pN',          cmd('editor.action.marker.nextInFiles'),opts)
+map('n', '<leader>pp',          cmd('editor.action.marker.prev'),       opts)
+map('n', '<leader>pP',          cmd('editor.action.marker.prevInFiles'),opts)
+map('n', '<leader>pe',          cmd('next-error.next.error'),           opts)
+map('n', '<leader>pE',          cmd('next-error.nextInFiles.error'),    opts)
+map('n', '<leader>pw',          cmd('next-error.next.warning'),         opts)
+map('n', '<leader>pW',          cmd('next-error.nextInFiles.warning'),  opts)
